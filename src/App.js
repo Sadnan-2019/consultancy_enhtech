96b912fcae5c2440846d34c7699e91a4fca224e4
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-06-23 14:16:29 +0600

    temporary all done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import Headerone from './components/Headerone/Headerone';
 import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
+import About from './components/About/About';
+import Business from './components/Business/Business';
+import Specialists from './components/Specialists/Specialists';
+import Projects from './components/Projects/Projects';
+import Testimonials from './components/Testimonials/Testimonials';
+import RecentProjects from './components/Projects/RecentProjects';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +17,13 @@ function App() {
       <Headerone></Headerone>
        <Header></Header>
        <Banner></Banner>
+       <About></About>
+       <Business></Business>
+       <Specialists></Specialists>
+       <Projects></Projects>
+       <Testimonials></Testimonials>
+       <RecentProjects></RecentProjects>
+       <Footer></Footer>
    
     </div>
   );
---
+++ src/components/Specialists/Specialists.js
@@ -0,0 +1,147 @@
+import React from "react";
+
+const Specialists = () => {
+  return (
+    <section id="ts-service-area" class="ts-service-area pb-0">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-12">
+            <h2 class="section-title">We Are Specialists In</h2>
+            <h3 class="section-sub-title">What We Do</h3>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="ts-service-box d-flex">
+              <div class="ts-service-box-img">
+                <img
+                  loading="lazy"
+                  src="images/icon-image/service-icon1.png"
+                  alt="service-icon"
+                />
+              </div>
+              <div class="ts-service-box-info">
+                <h3 class="service-box-title">
+                  <a href="#">Home Construction</a>
+                </h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipiscing elit Integer
+                  adipiscing erat
+                </p>
+              </div>
+            </div>
+
+            <div class="ts-service-box d-flex">
+              <div class="ts-service-box-img">
+                <img
+                  loading="lazy"
+                  src="images/icon-image/service-icon2.png"
+                  alt="service-icon"
+                />
+              </div>
+              <div class="ts-service-box-info">
+                <h3 class="service-box-title">
+                  <a href="#">Building Remodels</a>
+                </h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipiscing elit Integer
+                  adipiscing erat
+                </p>
+              </div>
+            </div>
+
+            <div class="ts-service-box d-flex">
+              <div class="ts-service-box-img">
+                <img
+                  loading="lazy"
+                  src="images/icon-image/service-icon3.png"
+                  alt="service-icon"
+                />
+              </div>
+              <div class="ts-service-box-info">
+                <h3 class="service-box-title">
+                  <a href="#">Interior Design</a>
+                </h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipiscing elit Integer
+                  adipiscing erat
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 text-center">
+            <img
+              loading="lazy"
+              class="img-fluid"
+              src="images/services/service-center.jpg"
+              alt="service-avater-image"
+            />
+          </div>
+
+          <div class="col-lg-4 mt-5 mt-lg-0 mb-4 mb-lg-0">
+            <div class="ts-service-box d-flex">
+              <div class="ts-service-box-img">
+                <img
+                  loading="lazy"
+                  src="images/icon-image/service-icon4.png"
+                  alt="service-icon"
+                />
+              </div>
+              <div class="ts-service-box-info">
+                <h3 class="service-box-title">
+                  <a href="#">Exterior Design</a>
+                </h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipiscing elit Integer
+                  adipiscing erat
+                </p>
+              </div>
+            </div>
+
+            <div class="ts-service-box d-flex">
+              <div class="ts-service-box-img">
+                <img
+                  loading="lazy"
+                  src="images/icon-image/service-icon5.png"
+                  alt="service-icon"
+                />
+              </div>
+              <div class="ts-service-box-info">
+                <h3 class="service-box-title">
+                  <a href="#">Renovation</a>
+                </h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipiscing elit Integer
+                  adipiscing erat
+                </p>
+              </div>
+            </div>
+
+            <div class="ts-service-box d-flex">
+              <div class="ts-service-box-img">
+                <img
+                  loading="lazy"
+                  src="images/icon-image/service-icon6.png"
+                  alt="service-icon"
+                />
+              </div>
+              <div class="ts-service-box-info">
+                <h3 class="service-box-title">
+                  <a href="#">Safety Management</a>
+                </h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipiscing elit Integer
+                  adipiscing erat
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Specialists;
