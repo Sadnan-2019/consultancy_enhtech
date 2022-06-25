239ec6a18b9a8b5e553a9f35ff0adfb7a6a4e869
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-06-25 16:11:30 +0600

    debagging runnig testimoniel

--- public/js/script.js
+++ public/js/script.js
@@ -228,8 +228,7 @@ jQuery(function ($) {
 				autoplay: true,
 				speed: 600,
 				arrows: true,
-				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
-				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+ 				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
 			});
 			$('.testimonial-slide').slickAnimation();
 		}
