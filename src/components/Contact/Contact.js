7de9887ccbaef2cec662c8532e99638ec8396625
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-06-30 02:09:40 +0600

    again done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -46,42 +46,43 @@ const Contact = () => {
 
 
 
-<section id="main-container" class="main-container">
-  <div class="container">
+<section id="main-container" class="main-container contact    ">
+ 
+ <div class="container-fluid ">
 
-   
+  <h3 class="column-title text-center mt-2" style={{color:"white"}}>We love to hear</h3>
 
-    <div class="row">
-    <h3 class="column-title text-center mt-2" style={{color:"#BC4B26"}}>We love to hear</h3>
-      <div class="col-md-12 justify-content-center align-items-center d-flex">
+
+    <div class="row ">
+      <div class="col-md-12 justify-content-center align-items-center d-flex p-4  ">
         
      
-        <form id="contact-form"  method="post"   onSubmit={sendEmail} className=" p-4 border rounded-4 contact" >
-          <div class="error-container"></div>
+        <form id="contact-form"  method="post"   onSubmit={sendEmail} className=" p-4 mx-auto   border rounded-4  contactform" >
+           
           <div class="row">
-            <div class="col-md-4">
+            <div class=" ">
               <div class="form-group">
-                <label>Name</label>
-                <input class="form-control "   name="name" id="name" placeholder="" type="text" required />
+                <label  >Name</label>
+                <input class="form-control   "   name="name" id="name" placeholder=" " type="text" required />
               </div>
             </div>
-            <div class="col-md-4">
+            <div class="  ">
               <div class="form-group">
-                <label>Email</label>
-                <input class="form-control form-control-email" name="email" id="email" placeholder="" type="email"
+                <label  >Email</label>
+                <input class="form-control   form-control-email" name="email" id="email" placeholder="" type="email"
                   required />
               </div>
             </div>
-            <div class="col-md-4">
+            <div class=" ">
               <div class="form-group">
-                <label>Subject</label>
-                <input class="form-control form-control-subject" name="subject" id="subject" placeholder="" type="text"   />
+                <label  >Subject</label>
+                <input class="form-control   form-control-subject" name="subject" id="subject" placeholder="" type="text"   />
               </div>
             </div>
           </div>
           <div class="form-group">
-            <label>Message</label>
-            <textarea class="form-control form-control-message" type="text" name="message" id="message" placeholder="" rows="10"
+            <label  >Message</label> 
+            <textarea class="form-control form-control-message   " type="text" name="message" id="message" placeholder="" rows="10"
               required></textarea>
           </div>
           <div class="text-right"><br></br>
@@ -93,6 +94,12 @@ const Contact = () => {
 
     </div> 
   </div> 
+  <div className='align-items-center justify-content-center d-flex p-4'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.1529895842637!2d90.37355931434828!3d23.74192319501847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b95463d9cc49%3A0x26f7bc45c034c899!2sDhanmondi!5e0!3m2!1sen!2sbd!4v1656438858353!5m2!1sen!2sbd" style={{width:"100%",height:"450px",border:"0"}}   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+ </div>
+
+
+ 
 </section>
 
 
@@ -105,9 +112,7 @@ const Contact = () => {
 </div>
 
 <div class="gap-40"></div> */}
-<div className='align-items-center justify-content-center d-flex p-4'>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.1529895842637!2d90.37355931434828!3d23.74192319501847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b95463d9cc49%3A0x26f7bc45c034c899!2sDhanmondi!5e0!3m2!1sen!2sbd!4v1656438858353!5m2!1sen!2sbd" style={{width:"100%",height:"450px",border:"0"}}   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
- </div>
+
           </div>
      );
 };
