d904b17cfb7daf27bf316fc054a763812218345e
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-07-05 14:13:04 +0600

    fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -28,7 +28,7 @@ const Contact = () => {
 
 
 <section id="main-container" class="main-container contact    ">
-  <h1 className='text-center text-light banner-title mt-5'>Contact</h1>
+  <h1 className='text-center text-light banner-title mt-5 pt-5 '  >Contact</h1>
 
  
  <div class="container-fluid ">
@@ -76,10 +76,17 @@ const Contact = () => {
       </div>
 
     </div> 
-  </div> 
+      
   <div className='align-items-center justify-content-center d-flex p-4'>
 <iframe className='broder rounded-3' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.1529895842637!2d90.37355931434828!3d23.74192319501847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b95463d9cc49%3A0x26f7bc45c034c899!2sDhanmondi!5e0!3m2!1sen!2sbd!4v1656438858353!5m2!1sen!2sbd" style={{width:"100%",height:"450px",border:"0"}}   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
  </div>
+  </div> 
+
+
+{/*   
+  <div className='align-items-center justify-content-center d-flex p-4'>
+<iframe className='broder rounded-3' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.1529895842637!2d90.37355931434828!3d23.74192319501847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b95463d9cc49%3A0x26f7bc45c034c899!2sDhanmondi!5e0!3m2!1sen!2sbd!4v1656438858353!5m2!1sen!2sbd" style={{width:"100%",height:"450px",border:"0"}}   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+ </div> */}
 
 
  
@@ -87,14 +94,7 @@ const Contact = () => {
 
 
 
-{/* <div class="gap-60"></div>
-
-<div class="google-map">
-  <div id="map" class="map" data-latitude="40.712776" data-longitude="-74.005974" data-marker="image
-  s/marker.png" data-marker-name="Constra"></div>
-</div>
-
-<div class="gap-40"></div> */}
+ 
 
           </div>
      );
